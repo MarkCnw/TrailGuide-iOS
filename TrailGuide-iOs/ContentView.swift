cf92f1b89a154545a2972638fc028f00567d66cc
MarkCnw <chinnawong554@gmail.com>
2026-04-11 16:24:24 +0700

    Initial Commit

---
+++ TrailGuide-iOs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrailGuide-iOs
+//
+//  Created by MarkCnw on 11/4/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrailGuide-iOs/TrailGuide_iOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailGuide_iOsApp.swift
+//  TrailGuide-iOs
+//
+//  Created by MarkCnw on 11/4/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct TrailGuide_iOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
